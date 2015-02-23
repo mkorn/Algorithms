61c591a6f90bd404beb6ad36c8b35defa7fc96ae
mkorn <mkorn@users.noreply.github.com>
2015-02-24 00:50:19 +0300

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+	int n, m;
+	scanf("%d%d", &n, &m);
+	if(n<0 || m<0)
+	{
+		n=abs(n);
+		m=abs(m);
+	}
+	while(n!=0 && m!=0)
+	{
+		if(n>m)
+			n%=m;
+		else
+			m%=n;
+	}
+	printf("%d", n+m);
+	
+	return 0;
+}
